b46cfade13d9d4320c4f31670dcae8d4a28c3406
lewis.ho@bytedance.com <lewis.ho@bytedance.com>
2025-05-19 13:36:19 +0800

    change link to embed

--- src/components/MiddleBlock/index.tsx
+++ src/components/MiddleBlock/index.tsx
@@ -129,7 +129,7 @@ const YouTubeEmbed = () => {
         }}
       >
         <iframe
-          src="https://www.youtube.com/watch?v=yqo8Jg4s2KE"
+          src="https://www.youtube.com/embed/yqo8Jg4s2KE?si=A0ToV9HiKBF-1FT1"
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
