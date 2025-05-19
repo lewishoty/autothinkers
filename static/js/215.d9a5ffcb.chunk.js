48b1e32a98c8928761591b688974e6401e888214
lewis.ho@bytedance.com <lewis.ho@bytedance.com>
2025-05-19 13:36:39 +0800

    Updates

---
+++ static/js/215.d9a5ffcb.chunk.js
@@ -0,0 +1,20 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[215],{4215:(e,s,i)=>{i.r(s),i.d(s,{default:()=>m});var d=i(1319),r=i(4647),t=i(9281),c=i(3614),n=i(9);(0,n.Ay)("section")`
+  position: relative;
+  padding: 7.5rem 0 3rem;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+
+  @media screen and (max-width: 1024px) {
+    padding: 5.5rem 0 3rem;
+  }
+`;const l=(0,n.Ay)("p")`
+  padding: 0.75rem 0 0.75rem;
+`,a=(0,n.Ay)("div")`
+  max-width: 570px;
+
+  @media only screen and (max-width: 768px) {
+    max-width: 100%;
+  }
+`;var o=i(2026),h=i(2646),x=i(579);const m=(0,t.C)()((e=>{let{title:s,content:i,button:t,t:n,id:m}=e;return(0,x.jsx)(o.Fe,{id:m,children:(0,x.jsxs)(c.zW,{triggerOnce:!0,children:[(0,x.jsx)(d.A,{justify:"center",align:"middle",children:(0,x.jsx)(a,{children:(0,x.jsx)(r.A,{lg:24,md:24,sm:24,xs:24,children:(0,x.jsx)("h5",{children:"What we can do for you"})})})}),(0,x.jsxs)("div",{className:"roworcol",children:[(0,x.jsx)(r.A,{lg:8,md:8,sm:8,xs:8,children:(0,x.jsx)(a,{children:(0,x.jsxs)("div",{className:"card",children:[(0,x.jsx)(h.A,{src:"excel.svg",width:"25%",height:"25%"}),(0,x.jsx)("h5",{children:"Data Entry"}),(0,x.jsx)(l,{className:"card-content",children:"Take data collected from stockcounting, reports, websites and organise them into your software of choice"})]})})}),(0,x.jsx)(r.A,{lg:8,md:8,sm:8,xs:8,children:(0,x.jsx)(a,{children:(0,x.jsxs)("div",{className:"card",children:[(0,x.jsxs)("div",{className:"icon-row",children:[(0,x.jsx)(h.A,{src:"icons8-linkedin.svg",width:"25%",height:"25%"}),(0,x.jsx)(h.A,{src:"icons8-indeed.svg",width:"25%",height:"25%"})]}),(0,x.jsx)("h5",{children:"Client Prospecting"}),(0,x.jsx)(l,{className:"card-content",children:"Search through linkedin or client's websites for keywords to create an email list or for cold calls"})]})})}),(0,x.jsx)(r.A,{lg:8,md:8,sm:8,xs:8,children:(0,x.jsx)(a,{children:(0,x.jsxs)("div",{className:"card",children:[(0,x.jsxs)("div",{className:"icon-row",children:[(0,x.jsx)(h.A,{src:"icons8-facebook.svg",width:"25%",height:"25%"}),(0,x.jsx)(h.A,{src:"icons8-tiktok.svg",width:"25%",height:"25%"}),(0,x.jsx)(h.A,{src:"icons8-sap.svg",width:"25%",height:"25%"})]}),(0,x.jsx)("h5",{children:"Leads Management"}),(0,x.jsx)(l,{className:"card-content",children:"Manage leads from facebook, tiktok, google ads and automatically generate a report"})]})})})]}),(0,x.jsx)(d.A,{justify:"center",align:"middle",children:(0,x.jsx)(a,{children:(0,x.jsx)(r.A,{lg:24,md:24,sm:24,xs:24,children:(0,x.jsx)("h5",{children:(0,x.jsx)("i",{children:(0,x.jsx)("u",{children:"Heres an example..."})})})})})}),(0,x.jsx)(j,{})]})})})),j=()=>(0,x.jsx)("div",{style:{display:"flex",justifyContent:"center",padding:"2rem"},children:(0,x.jsx)("div",{style:{position:"relative",width:"100%",maxWidth:"800px",paddingTop:"56.25%"},children:(0,x.jsx)("iframe",{src:"https://www.youtube.com/embed/yqo8Jg4s2KE?si=A0ToV9HiKBF-1FT1",title:"YouTube video player",frameBorder:"0",allow:"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",allowFullScreen:!0,style:{position:"absolute",top:0,left:0,width:"100%",height:"100%"}})})})}}]);
+//# sourceMappingURL=215.d9a5ffcb.chunk.js.map
